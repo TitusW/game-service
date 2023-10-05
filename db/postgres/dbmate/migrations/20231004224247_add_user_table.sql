a5c43b95fac29ae1e295d5feb03ed1ef46fff242
titus <tituswus@gmail.com>
2023-10-05 17:03:41 +0700

    New migration add user table

--- db/postgres/dbmate/migrations/20231004224247_add_user_table.sql
+++ db/postgres/dbmate/migrations/20231004224247_add_user_table.sql
@@ -1,5 +1,15 @@
 -- migrate:up
+CREATE SCHEMA IF NOT EXISTS "game";
 
+CREATE TABLE IF NOT EXISTS "game"."users" (
+  "ksuid" varchar(255) PRIMARY KEY NOT NULL,
+  "email" varchar(255),
+  "password" varchar(255),
+  "inserted_at" timestamptz NOT NULL,
+  "updated_at" timestamptz NOT NULL,
+  "deleted_at" timestamptz NOT NULL
+);
 
 -- migrate:down
+DROP TABLE IF EXISTS "game"."users";
 
